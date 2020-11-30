ceed309efb25428d9c8ac8a1e64f1b8e9d1dff2d
Bon Lacorte <bonbonlacorte123@gmail.com>
2020-11-30 08:45:38 +0800

    first

---
+++ app.js
@@ -0,0 +1,42 @@
+const question = [
+    {
+        question: 'Which country held the 2016 Summer Olympics?',
+        a: 'China',
+        b: 'Ireland',
+        c: 'Brazil',
+        d: 'Italy',
+        correct: 'c'
+    },
+    {
+        question: 'What is the most used programming language in 2019?',
+        a: 'Java',
+        b: 'C',
+        c: 'Python',
+        d: 'Javascript',
+        correct: 'a'
+    },
+    {
+        question: "What is the color of Donald Duck's bowtie?",
+        a: 'Red',
+        b: 'Yellow',
+        c: 'Blue',
+        d: 'White',
+        correct: 'a'
+    },
+    {
+        question: "What was the name of the band Lionel Richie was a part of?",
+        a: 'King Harvest',
+        b: 'Spectrums',
+        c: 'Commodores',
+        d: 'The Marshall Tucker band',
+        correct: 'c'
+    }
+    {
+        question: 'Which animal does not appear in the Chinese zodiac?',
+        a: 'Dragon',
+        b: 'Rabbit',
+        c: 'Dog',
+        d: 'Hummingbird',
+        correct: 'd'
+    }
+]
